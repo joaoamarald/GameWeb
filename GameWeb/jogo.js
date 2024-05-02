4cc4e13d049f35559f75df30a4c86cc9c445c477
joaoamarald <joaopedroamaral6565@gmail.com>
2024-05-02 09:57:13 -0300

    Função de Redimensionamento

---
+++ GameWeb/jogo.js
@@ -0,0 +1,13 @@
+// base para uma função de um evento de redimensionamento atualizado
+
+var altura = 0
+var largura = 0
+
+function ajustarTamanhoPalcoJogo() {
+    altura = window.innerHeight
+    largura = window.innerWidth
+
+    console.log(altura, largura)
+}
+
+ajustarTamanhoPalcoJogo()
